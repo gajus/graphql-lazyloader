0a7180ade5525c48024fe7bca8738117c060c0b2
Gajus Kuizinas <gajus@gajus.com>
2020-11-12 23:14:08 -0600

    test: check compatibility with dataloader

--- test/graphql-lazyloader/lazyLoadDirective.ts
+++ test/graphql-lazyloader/lazyLoadDirective.ts
@@ -2,6 +2,7 @@ import {
   gql,
 } from 'apollo-server-express';
 import test from 'ava';
+import DataLoader from 'dataloader';
 import request from 'graphql-request';
 import sinon from 'sinon';
 import createGraphqlServer from '../helpers/createGraphqlServer';
@@ -223,4 +224,83 @@ test('does not fetch already available data', async (t) => {
   await graphqlServer.stop();
 
   t.is(lazyLoad.callCount, 0);
+});
+
+test('batches multiple requests', async (t) => {
+  const lazyLoad = sinon
+    .stub()
+    .resolves([
+      {
+        id: '1',
+        name: 'foo',
+      },
+      {
+        id: '2',
+        name: 'bar',
+      },
+    ]);
+
+  const fooLoader = new DataLoader(lazyLoad);
+
+  const graphqlServer = await createGraphqlServer({
+    resolvers: {
+      Foo: {
+        __lazyLoad: ({ id }) => {
+          return fooLoader.load(id);
+        },
+      },
+      Query: {
+        foos: () => {
+          return [
+            {
+              id: '1',
+            },
+            {
+              id: '2',
+            },
+          ];
+        },
+      },
+    },
+    typeDefs: gql`
+      directive @lazyLoad on OBJECT
+
+      type Foo @lazyLoad {
+        id: ID!
+        name: String!
+      }
+
+      type Query {
+        foos: [Foo!]!
+      }
+    `,
+  });
+
+  const response = await request(graphqlServer.url, gql`
+    {
+      foos {
+        id
+        name
+      }
+    }
+  `);
+
+  t.deepEqual(response, {
+    foos: [
+      {
+        id: '1',
+        name: 'foo',
+      },
+      {
+        id: '2',
+        name: 'bar',
+      },
+    ],
+  });
+
+  await graphqlServer.stop();
+
+  t.deepEqual(lazyLoad.firstCall.firstArg, ['1', '2']);
+
+  t.is(lazyLoad.callCount, 1);
 });
